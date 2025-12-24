2d6f65df1a976a73fd689ea2cbd9ae8ea9c1103a
Josss4141 <josue.ec.4411@gmail.com>
2025-12-23 22:39:59 -0500

    Agregando script de reinicio de DB

---
+++ reset_db.py
@@ -0,0 +1,25 @@
+from app import create_app, db
+from app.models import Usuario
+from werkzeug.security import generate_password_hash
+
+app = create_app()
+
+with app.app_context():
+    # 1. BORRAMOS TODO (Esto elimina el conflicto de la tabla vieja)
+    db.drop_all()
+    print("🗑️ Base de datos antigua borrada.")
+
+    # 2. CREAMOS TODO NUEVO (Con las columnas de precio y costo)
+    db.create_all()
+    print("✨ Tablas nuevas creadas.")
+
+    # 3. CREAMOS AL ADMIN
+    usuario = "admin"
+    password_plano = "admin123" 
+    password_segura = generate_password_hash(password_plano)
+    
+    nuevo_admin = Usuario(nombre="Administrador", username=usuario, password=password_segura)
+    db.session.add(nuevo_admin)
+    db.session.commit()
+    
+    print("✅ Usuario Admin restaurado.")
